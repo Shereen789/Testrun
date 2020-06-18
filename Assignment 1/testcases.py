ebf94c55906a87af002ff6d4af4c48b30ddba0c7
Shereen789 <irfanashereen@msitprogram.net>
2020-06-18 13:14:39 +0530

    Jun 18  13:14:39

--- Assignment 1/testcases.py
+++ Assignment 1/testcases.py
@@ -1,8 +1,7 @@
 import unittest
-from A1 import *
-​
-class Testing(unittest.TestCase) :
-    def test_add(self) :
+from Operations import *
+class Testing(unittest.TestCase):
+    def test_add(self):
         self.assertEqual(maxBlock("hoopla"), 2)
         self.assertEqual(maxBlock("abbCCCddBBBxx"), 3)
         self.assertEqual(maxBlock(""), 0)
@@ -12,6 +11,5 @@ class Testing(unittest.TestCase) :
         self.assertEqual(maxBlock("XXBBBbbxx"), 3)
         self.assertEqual(maxBlock("XXBBBBbbxx"), 4)
         self.assertEqual(maxBlock("XX2222BBBbbXX2222"), 4)
-​
-if __name__=="__main__" :
-    unittest.main()
+    if if __name__ == "__main__":
+        unittest.main()
