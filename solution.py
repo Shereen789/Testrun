121f3fdc32270efdc3105f704fa991555839791c
Shereen789 <irfanashereen@msitprogram.net>
2020-07-02 17:33:26 +0530

    Jul 02  17:33:26

---
+++ solution.py
@@ -0,0 +1,14 @@
+def maxBlock(word):
+    #a sample program
+    m= 1
+    ma = 1
+    for i in range(0,len(word)-1):
+        if word[i] == word[i+1]:
+            m+=1
+            if m>ma:
+                ma = m
+        else:
+            m = 1
+    if len(word) == 0:
+        ma = 0
+    return ma
